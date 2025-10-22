3bac0b60683e479e040e68c08dae5ef9020b8f16
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-10-22 11:48:41 +0300

    Delete The_program_of_mathematical_calculations.py

--- The_program_of_mathematical_calculations.py
+++
@@ -1,9 +0,0 @@
-number=int(input("Enter a four-digit number: "))
-thousands=number//1000
-hundreds=(number//100)%10
-tens=(number//10)%10
-units=number%10
-print(f'Цифра в позиции тысяч равна {thousands}')
-print(f'Цифра в позиции сотен равна {hundreds}')
-print(f'Цифра в позиции десятков равна {tens}')
-print(f'Цифра в позиции единиц равна {units}')
