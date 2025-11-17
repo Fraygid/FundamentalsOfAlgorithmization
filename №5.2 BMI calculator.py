c02dffa70b74e2e627544048d67310703a03119c
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-11-17 12:16:43 +0300

    Update №5.2 BMI calculator.py

--- №5.2 BMI calculator.py
+++ №5.2 BMI calculator.py
@@ -1,6 +1,6 @@
 def calculate_bmi():
-    print("Enter your weight(kg) and height(m) separated by space: ")
+    print("Введите свой вес (кг) и рост (м), разделяя их пробелом: ")
     weight, height = map(float, input().split())
     bmi = weight/(height**2)
-    print(f'Your BMI index is {bmi:.1f}')
+    print(f'Ваш индекс ИМТ {bmi:.1f}')
 calculate_bmi()
