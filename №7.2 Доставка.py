6fda6832cd8db9bee87fb7f8e5e6912b1cc0b8a7
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-11-24 11:40:13 +0300

    Update №7.2 Доставка.py

--- №7.2 Доставка.py
+++ №7.2 Доставка.py
@@ -1,4 +1,4 @@
-order_status=input('Введите состояние заказа(pending, processing, shipped, delivered, cancelled')
+order_status=input('Введите состояние заказа(pending, processing, shipped, delivered, cancelled): ')
 
 match order_status:
     case 'pending':
@@ -8,7 +8,7 @@ match order_status:
         waiting_time='1-2 часа'
     case 'processing':
         status='В обработке'
-        description='Закза готовиться к отправке'
+        description='Заказ готовиться к отправке'
         emoji='📦'
         waiting_time='1-3 дня'
     case 'shipped':
@@ -16,3 +16,19 @@ match order_status:
         description='Заказ передан в службу доставки'
         emoji='🚚'
         waiting_time='3-7 дней'
+    case 'delivered':
+        status='Доставлено'
+        description='Заказ доставлен получателю'
+        emoji='✅'
+        waiting_time='Заказ завершен'
+    case 'cancelled':
+        status='Отменено'
+        description='Заказ был отменен'
+        emoji='❌'
+        waiting_time='Обработка отмены: 1-24 часа'
+    case _:
+        print('❌ Ошибка: неизвестный статус заказа')
+print(f'Статус заказа: {status}')
+print(f'Описание заказа: {description}')
+print(emoji)
+print(f'Время обработки: {waiting_time}')
