3c3d15c6f4e3af9fa6c9934783239115babf0996
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-12-01 12:19:59 +0300

    Update №8.5 Наибольшие числа.py

--- №8.5 Наибольшие числа.py
+++ №8.5 Наибольшие числа.py
@@ -7,7 +7,7 @@ if num < 2:
     print('Число должно быть больше 2')
 else:
     for i in range(num):
-        x = int(input(f'Введите число {i+1}: '))
+        x = int(input(f'Введите число №{i+1}: '))
         if x > first:
             second = first
             first = x
