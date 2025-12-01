9b2bc6566294cf7606cb92a6a9dac6988ee390da
Fraygid <136465592+Fraygid@users.noreply.github.com>
2025-12-01 11:58:48 +0300

    Update №8.6 Угадать число за 3 попытки.py

--- №8.6 Угадать число за 3 попытки.py
+++ №8.6 Угадать число за 3 попытки.py
@@ -4,7 +4,7 @@ secret = random.randint(1,10)
 guessed = False
 
 for i in range(1, 4):
-    guess = int(input('Введите попытку угадки числа(от 1 до 10): '))
+    guess = int(input('Введите попытку угадывания числа(от 1 до 10): '))
     if guess == secret:
         print('Угадали!')
         guessed = True
